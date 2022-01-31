eb5a396dfae2b1e3a91f57c5a16abb3bfdf41a90
arvalea <arvalea@gmail.com>
2022-01-31 14:27:53 +0100

    ssr simulation

--- react.js
+++ react.js
@@ -1,4 +1,14 @@
 import { render } from 'react-dom';
+import { renderToString } from 'react-dom/server';
+import { hydrate } from 'react-dom';
 import Wordle from './wordle';
 
 render(<Wordle />, document.getElementById('root'));
+
+// SSR simulation
+
+// document.getElementById('root').innerHTML = renderToString(<Wordle />);
+
+// setTimeout(() => {
+//   hydrate(<Wordle />, document.getElementById('root'));
+// }, 1500);
